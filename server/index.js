14e62ed56243507ba8e72addae2790abf6414a2c
Tej <157304155+TejDabhi@users.noreply.github.com>
2026-05-22 17:27:14 +0530

    Allow CORS from any origin

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const app = express()
 
 app.use(express.json())
 app.use(cors({
-    origin: "https://login-mern-mauve.vercel.app",
+    origin: "*",
     methods:['GET','POST'],
     credentials:true
 }))
