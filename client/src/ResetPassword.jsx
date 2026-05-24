9a1c6effe7984ddb1aed3ee73ac2c1d864481a67
Tej <157304155+TejDabhi@users.noreply.github.com>
2026-05-24 07:35:30 +0530

    Add useNavigate for password reset navigation

--- client/src/ResetPassword.jsx
+++ client/src/ResetPassword.jsx
@@ -1,10 +1,12 @@
 import axios from 'axios'
 import React, { useState } from 'react'
 import { useParams } from 'react-router-dom'
+import { useNavigate } from 'react-router-dom'
 
 const ResetPassword = () => {
 
     const [password, setPassword] = useState('')
+    const navigate = useNavigate()
 
     const { id, token } = useParams()
 
